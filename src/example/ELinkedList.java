775902d721df95fd557d4c6577b2ab57d8dfd88d
Anton <Intemic@gmail.com>
2016-11-23 16:28:34 +0500

    Тестирование

---
+++ src/example/ELinkedList.java
@@ -0,0 +1,10 @@
+package example;
+
+import java.util.LinkedList;
+import java.util.SortedSet;
+
+/**
+ * Created by Anton on 23.11.2016.
+ */
+//public class ELinkedList extends LinkedList implements SortedSet {
+//}
