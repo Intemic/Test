6b8b4b03c1ded20a2a285f00092b4b4831ee75f0
Anton <Intemic@gmail.com>
2016-11-24 23:20:53 +0500

    Пакеты

--- src/example/EWords.java
+++ src/example/EWords.java
@@ -1,9 +1,11 @@
 package example;
 
 import containers.AssociativeArray;
+import containers.SlowMap;
 import net.mindview.util.TextFile;
 
 import java.util.ArrayList;
+import java.util.Map;
 
 /**
  * Created by Anton on 24.11.2016.
@@ -11,20 +13,30 @@ import java.util.ArrayList;
 public class EWords {
     public static void main(String[] arg) {
         ArrayList<String> al = new TextFile("C:\\Users\\Anton\\IdeaProjects\\JavaBook\\src\\example\\text.txt", "\\s");
-        AssociativeArray<String, Integer> aa = new AssociativeArray<String, Integer>(10);
+        SlowMap<String, Integer> aa = new SlowMap<String, Integer>();
         for (String s : al) {
           // System.out.println(s);
 
-            try {
-
-                aa.put(s, aa.get(s) + 1);
+//            try {
+                if (aa.get(s) == null)
+                    aa.put(s, 1);
+                else
+                    aa.put(s, aa.get(s) + 1);
+/*
             } catch (ArrayIndexOutOfBoundsException e) {
                 System.out.println("Error");
                 break;
             }
-
+*/
         }
-        System.out.println(al.toString());
+
+
+        for(Map.Entry<String, Integer> mp : aa.entrySet()){
+          System.out.println("[" + mp.getKey() + "=" + mp.getValue() +"]");
+        }
+
+
+       // System.out.println(al);
     }
 
 }
