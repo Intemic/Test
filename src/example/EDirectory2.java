11c2dab4b1480db9b13af14e249eca22faf81ed9
Anton <Intemic@gmail.com>
2016-12-13 14:58:48 +0500

    Примеры программ

--- src/example/EDirectory2.java
+++ src/example/EDirectory2.java
@@ -1,27 +1,32 @@
 package example;
 
 import net.mindview.util.ProcessFiles;
+
 import java.io.File;
+import java.util.Arrays;
+import java.util.regex.Pattern;
 
 /**
  * Created by Anton on 13.12.2016.
  */
 public class EDirectory2 {
-  public static long lgo = 0;
+    public static long lgo = 0;
 
-  public static void main(String[] arg){
-      String[] st = new String[1];
 
-      st[0] = arg[0];
+    public static void main(final String[] arg) {
+        String[] st = new String[1];
+        st[0] = arg[0];
 
-     new ProcessFiles(new ProcessFiles.Strategy() {
-         public void process(File file){
-           lgo += file.length();
-         }
-     }, arg[1]).start(st);
+        new ProcessFiles(new ProcessFiles.Strategy() {
+            private Pattern pt = Pattern.compile(arg[2]);
 
-     //pf.start(st);
+            public void process(File file) {
+                if (pt.matcher(file.getName()).matches())
+                    lgo += file.length();
+            }
+        }, arg[1]).start(st);
 
-     System.out.print("Total size - " + lgo);
-  }
+        System.out.print("Total size - " + lgo);
+    }
 }
+
