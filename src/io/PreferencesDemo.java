63621f787c844b841ad2aacc7e17bc3fc5c82d93
Anton <Intemic@gmail.com>
2017-01-12 20:18:32 +0500

    Пакеты

--- src/io/PreferencesDemo.java
+++ src/io/PreferencesDemo.java
@@ -1,24 +1,31 @@
-//: io/PreferencesDemo.java
+package io;//: io/PreferencesDemo.java
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.prefs.*;
+
 import static net.mindview.util.Print.*;
 
 public class PreferencesDemo {
-  public static void main(String[] args) throws Exception {
-    Preferences prefs = Preferences
-      .userNodeForPackage(PreferencesDemo.class);
-    prefs.put("Location", "Oz");
-    prefs.put("Footwear", "Ruby Slippers");
-    prefs.putInt("Companions", 4);
-    prefs.putBoolean("Are there witches?", true);
-    int usageCount = prefs.getInt("UsageCount", 0);
-    usageCount++;
-    prefs.putInt("UsageCount", usageCount);
-    for(String key : prefs.keys())
-      print(key + ": "+ prefs.get(key, null));
-    // You must always provide a default value:
-    print("How many companions does Dorothy have? " +
-      prefs.getInt("Companions", 0));
-  }
+    public static void main(String[] args) throws Exception {
+        Preferences prefs = Preferences
+                .userNodeForPackage(PreferencesDemo.class);
+        prefs.put("Location", "Oz");
+        prefs.put("Footwear", "Ruby Slippers");
+        prefs.putInt("Companions", 4);
+        prefs.putBoolean("Are there witches?", true);
+        int usageCount = prefs.getInt("UsageCount", 0);
+        usageCount++;
+        prefs.putInt("UsageCount", usageCount);
+        for (String key : prefs.keys())
+            print(key + ": " + prefs.get(key, null));
+        // You must always provide a default value:
+        print("How many companions does Dorothy have? " +
+                prefs.getInt("Companions", 0));
+
+        HashMap
+    }
 } /* Output: (Sample)
 Location: Oz
 Footwear: Ruby Slippers
