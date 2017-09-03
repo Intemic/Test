cffb65888e2919e1dfc4f60c4ceb05cacdcdaf78
Anton <intemic@yandex.ru>
2017-09-03 10:06:00 +0500

    Дополнение

--- src/example/EFibonachiCalling.java
+++ src/example/EFibonachiCalling.java
@@ -26,25 +26,52 @@ class EFibonaiCallable extends Fibonacci implements Callable<String> {
         return "Object id - " + id + " Result : " + result;
     }
 
-    public static void main(String[] arg){
-      System.out.print("Run");
+    public static void main(String[] arg) {
+        System.out.print("Run");
     }
 }
 
 public class EFibonachiCalling {
-    public static void main(String[] arg) throws ExecutionException{
+    private static int counter = 0;
+
+    public static Future<String> runTask(final int count) {
+        ExecutorService ex = null;
+        Callable<String> cs = null;
+
+        if (ex == null) {
+            ex = Executors.newCachedThreadPool();
+
+            cs = new Callable<String>() {
+                private final int id = counter++;
+                private Fibonacci fb = new Fibonacci();
+
+                @Override
+                public String call() throws Exception {
+                    String result = "";
+                    for (int i = 0; i < count; i++)
+                        result += fb.next() + " ";
+                    return "Object id - " + id + " Result : " + result;
+                }
+            };
+        }
+
+        return ex.submit(cs);
+    }
+
+    public static void main(String[] arg) throws ExecutionException {
         Random rd = new Random(47);
-        ExecutorService ex = Executors.newCachedThreadPool();
+//        ExecutorService ex = Executors.newCachedThreadPool();
         ArrayList<Future<String>> al = new ArrayList<Future<String>>();
         for (int i = 0; i < 5; i++)
-            al.add(ex.submit(new EFibonaiCallable(rd.nextInt(25))));
-        for (Future<String> fs : al){
+//            al.add(ex.submit(new EFibonaiCallable(rd.nextInt(25))));
+            al.add(runTask(rd.nextInt(25)));
+        for (Future<String> fs : al) {
             try {
                 System.out.println(fs.get());
             } catch (InterruptedException e) {
                 e.printStackTrace();
             } finally {
-               ex.shutdown();
+//                ex.shutdown();
             }
         }
     }
